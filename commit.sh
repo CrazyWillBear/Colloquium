5ee74ede48d11d10ed2b37b2c702c63d487d0125
succulent <60blackandyellow60@gmail.com>
2021-01-21 22:39:49 -0800

    Test

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+###################################
+#      Linux commit script        #
+###################################
+
+# Remove built files:
+if [ -d "./colloq-server/bin" ]; then
+  rm -r ./colloq-server/bin
+fi
+if [ -d "./colloq-client/bin" ]; then
+  rm -r ./colloq-server/bin
+fi
+echo "Removed build files."
+
+git add ./*
+git commit -m "$1"
+echo "Commited files."
+
+git push -all
+echo "Pushed files."
